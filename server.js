1a41d0c60aa6f3d1215a04160bca4c6ca4f18edb
The Administrator <46427504+TheAdministrator1@users.noreply.github.com>
2024-01-26 12:41:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const http = require("http")
-const app = require("./app")
+import http from 'http'
+import app from './app'
+//const http = require("http")
+//const app = require("./app")
 
 const port = process.env.PORT || 3000;
 
